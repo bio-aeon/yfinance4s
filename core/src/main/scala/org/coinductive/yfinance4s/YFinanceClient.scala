369bbd912acadd3ae7421c494c4be5d095939e5f
Alexandr Chigrinets <alexandr@wps.su>
2024-12-30 00:16:10 +0200

    Separating data types for internal and external results

--- core/src/main/scala/org/coinductive/yfinance4s/YFinanceClient.scala
+++ core/src/main/scala/org/coinductive/yfinance4s/YFinanceClient.scala
@@ -1,21 +1,43 @@
 package org.coinductive.yfinance4s
 
+import cats.Functor
 import cats.effect.{Async, Resource}
-import org.coinductive.yfinance4s.models.{Interval, Range, Ticker, YFinanceQueryResult}
+import cats.syntax.functor._
+import org.coinductive.yfinance4s.models.{ChartResult, Interval, Range, Ticker, YFinanceQueryResult}
 
-import java.time.ZonedDateTime
+import java.time.{Instant, ZoneOffset, ZonedDateTime}
 
-final class YFinanceClient[F[_]] private (gateway: YFinanceGateway[F]) {
+final class YFinanceClient[F[_]: Functor] private (gateway: YFinanceGateway[F]) {
 
-  def getChart(ticker: Ticker, interval: Interval, range: Range): F[YFinanceQueryResult] =
-    gateway.getChart(ticker, interval, range)
+  def getChart(ticker: Ticker, interval: Interval, range: Range): F[Option[ChartResult]] =
+    gateway.getChart(ticker, interval, range).map(mapResult)
 
   def getChart(
       ticker: Ticker,
       interval: Interval,
       since: ZonedDateTime,
       until: ZonedDateTime
-  ): F[YFinanceQueryResult] = gateway.getChart(ticker, interval, since, until)
+  ): F[Option[ChartResult]] = gateway.getChart(ticker, interval, since, until).map(mapResult)
+
+  private def mapResult(result: YFinanceQueryResult): Option[ChartResult] = {
+    result.chart.result.headOption.map { data =>
+      val quotes = (0 until data.timestamp.length).map { i =>
+        val quote = data.indicators.quote.head
+        val adjclose = data.indicators.adjclose.head
+        ChartResult.Quote(
+          ZonedDateTime.ofInstant(Instant.ofEpochSecond(data.timestamp(i)), ZoneOffset.UTC),
+          quote.close(i),
+          quote.open(i),
+          quote.volume(i),
+          quote.high(i),
+          quote.low(i),
+          adjclose.adjclose(i)
+        )
+      }.toList
+
+      ChartResult(quotes)
+    }
+  }
 
 }
 
