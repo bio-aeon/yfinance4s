5d4ff9043756b9bf185e35badee9bab9304a184c
Alexandr Chigrinets <alexandr@wps.su>
2024-12-29 17:42:41 +0200

    Minor refactoring of Yahoo Finance gateway

--- core/src/main/scala/org/coinductive/yfinance4s/Gateway.scala
+++ core/src/main/scala/org/coinductive/yfinance4s/Gateway.scala
@@ -5,7 +5,7 @@ import cats.syntax.either._
 import cats.syntax.flatMap._
 import cats.syntax.show._
 import io.circe.parser.decode
-import sttp.client3.{SttpBackend, UriContext, basicRequest}
+import sttp.client3.{Response, SttpBackend, UriContext, basicRequest}
 
 trait Gateway[F[_]] {
   def getChart(ticker: String, interval: Interval, range: Range): F[YFinanceQueryResult]
@@ -23,26 +23,26 @@ object Gateway {
     def getChart(ticker: String, interval: Interval, range: Range): F[YFinanceQueryResult] = {
       val req =
         basicRequest.get(apiEndpoint.addPath(ticker).withParams(("interval", interval.show), ("range", range.show)))
-      req
-        .send(sttpBackend)
-        .flatMap { resp =>
-          if (resp.isSuccess) {
-            resp.body
-              .leftMap(x => new Exception(s"Inconsistent response: $x"))
+      req.send(sttpBackend).flatMap(parseResponse)
+    }
+
+    private def parseResponse(
+        response: Response[Either[String, String]]
+    ): F[YFinanceQueryResult] = {
+      if (response.isSuccess) {
+        response.body
+          .leftMap(r => new Exception(s"Inconsistent response: $r"))
+          .fold(
+            F.raiseError,
+            decode[YFinanceQueryResult](_)
               .fold(
-                F.raiseError,
-                decode[YFinanceQueryResult](_)
-                  .fold(
-                    e => F.raiseError(new Exception(s"Illegible response: ${e.getMessage}")),
-                    F.pure
-                  )
+                e => F.raiseError(new Exception(s"Illegible response: ${e.getMessage}")),
+                F.pure
               )
-          } else {
-            F.raiseError(
-              new Exception(s"Unexpected code: ${resp.code}. Details: ${resp.body.merge}")
-            )
-          }
-        }
+          )
+      } else {
+        F.raiseError(new Exception(s"Unexpected code: ${response.code}. Details: ${response.body.merge}"))
+      }
     }
   }
 
